8f74c4babff9da75af9e82c7fa393c068807ffa3
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2022-04-13 16:36:03 -0700

    add theme chakra example

---
+++ src/theme/index.ts
@@ -0,0 +1,16 @@
+import { extendTheme } from "@chakra-ui/react";
+
+import Heading from "./heading";
+
+export default extendTheme({
+  fonts: {
+    heading:
+      "Inter,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji",
+    body: "Inter,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji",
+    mono: "Menlo, monospace",
+  },
+  components: {
+    Heading,
+  },
+  colors: {},
+});
