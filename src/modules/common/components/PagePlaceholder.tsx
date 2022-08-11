7409a1f9ef67cbd48178c4c88bc63befb909cedd
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2022-08-11 23:35:05 +0530

    WIP

---
+++ src/modules/common/components/PagePlaceholder.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react'
+
+interface PagePlaceholderProps{
+    imageUrl:string;
+    description:string;
+}
+const PagePlaceholder: FC<PagePlaceholderProps> = (props) => {
+    const {} = props
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PagePlaceholder
