e4a76ad0e16af309dccf8213f7f638d77d33a987
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2022-10-03 18:53:50 +0530

    Added comments

--- src/modules/sdk/hooks/useConstructAppMsg.ts
+++ src/modules/sdk/hooks/useConstructAppMsg.ts
@@ -6,8 +6,12 @@ import { IAppContract } from "../types";
 import { constructMsg } from "../utils";
 
 /**
- * Provides a function to convert from a JSON Schema message to a valid Tx message
- * @returns A function to convert from a JSON Schema message to a valid Tx message
+ * Provides a function to create app construct messages. Apps are list of app component but
+ * instantiate msg (ie data of app component) is base64 encoded. We get a Record<panelId,panelData>
+ * from form-builder. So here we first get app data from system panel and create app-contract.
+ * Then we add panels to app list by encoding their data in base64. Ultimately, we return the msg
+ * generated which will then be used by andromeda client to process contract.
+ * Refer docs https://docs.andromedaprotocol.io/andromeda/andromeda-digital-objects/app#instantiatemsg 
  */
 export default function useConstructAppMsg() {
   const { registryAddress } = useAndromedaContext();
@@ -15,15 +19,19 @@ export default function useConstructAppMsg() {
   const constructAppMsg = useCallback(
     (data: ITemplateFormData) => {
       console.clear();
+
+      // Our system panel name is 'publish-settings'. Refer app template in schema
       const publishSettingsPanel = data['publish-settings'];
       if (!publishSettingsPanel || !publishSettingsPanel.name) throw new Error("Incorrect publish settings fields");
 
+      // App contract which we will use to initialise. We will add app components in app list here.
       const appContract: IAppContract = {
         name: publishSettingsPanel.name,
         app: [],
         primitive_contract: registryAddress
       };
 
+      // Traverse each panel
       Object.entries(data).forEach(([id, panel]) => {
         // Do not add system panels in app data
         if (panel.$class === 'system') return;
@@ -33,6 +41,7 @@ export default function useConstructAppMsg() {
         // Ado Type of current panel
         const adoType = panel.$type as IAdoType;
 
+        // Remove hidden fields from panel data
         const msg = constructMsg(panel);
 
         console.log(`Unencoded data for panel: ${id}`, msg);
