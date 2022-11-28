1b364f1ae799e6df84048af0977a07fe7156d92b
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2022-11-28 23:13:41 +0530

    Fixed spacing and typo error

--- src/modules/modals/components/DisclaimerModal/index.tsx
+++ src/modules/modals/components/DisclaimerModal/index.tsx
@@ -80,8 +80,7 @@ const DisclaimerModal: FC<DisclaimerModalProps> = ({ onAccept }) => {
           mt="1"
         />
         <Text fontSize="xs" fontWeight="light">
-          I understand and agree with the risks and disclaimers of using
-          Andromeda Protocol
+          I understand and agree with the risks and disclaimers of using Andromeda Protocol
         </Text>
       </HStack>
 
@@ -103,11 +102,11 @@ const DisclaimerModal: FC<DisclaimerModalProps> = ({ onAccept }) => {
 };
 
 const TERMS = [
-  `I have read and understood, and do hereby agree to be legally bound as a “User” under theTerms, including all future amendments thereto. Such agreement is irrevocable and will apply toall of my uses of the Site without me providing confirmation in each specific instance.`,
-  `I acknowledge and agree that the Site solely provides information about data on blockchainsin the Cosmos ecosystem. I accept that the Site operators have no custody over my funds,ability or duty to transact on my behalf, or power to reverse my transactions. The Site operatorsdo not endorse or provide any warranty with respect to any tokens.`,
-  `Andromeda Protocol is a decentralized blockchain where individuals or communities can usethe platform to build smart contracts, create their own templates to use or sell in themarketplace, create tokens & ADO’s, distribute digital assets, etc. These are all done byinstantiating, invoking, and querying smart contracts.`,
-  `Andromeda Protocol is made up of free, public, and open-source software that is built usingthe CosmosSDK / CosmWasm tech stack. Anyone can use our platform to build smartcontracts, create their own templates to use or sell in the marketplace, create tokens & ADO’s,distribute digital assets, etc. As a result, it is not possible to ensure all aspects of the platformare safe & secure to use. As with any experimental technology you are using it at your own risk.`,
-  `AS DESCRIBED IN THE DISCLAIMER, ANDROMEDA PROTOCOL IS PROVIDED “AS IS”,AT YOUR OWN RISK, AND WITHOUT ANY WARRANTIES OF ANY KIND. No developer orentity involved in creating Andromeda Protocol will be liable for any claims or damageswhatsoever associated with your use, inability to use, or your interaction with other users of theAndromeda Protocol, including any direct, indirect, incidental, special, exemplary, punitive orconsequential damages, or loss of profits, tokens, or anything else.`,
+  `I have read and understood, and do hereby agree to be legally bound as a “User” under the Terms, including all future amendments thereto. Such agreement is irrevocable and will apply to all of my uses of the Site without me providing confirmation in each specific instance.`,
+  `I acknowledge and agree that the Site solely provides information about data on blockchains in the Cosmos ecosystem. I accept that the Site operators have no custody over my funds, ability or duty to transact on my behalf, or power to reverse my transactions. The Site operators do not endorse or provide any warranty with respect to any tokens.`,
+  `Andromeda Protocol is a decentralized blockchain where individuals or communities can use the platform to build smart contracts, create their own templates to use or sell in the marketplace, create tokens & ADO’s, distribute digital assets, etc. These are all done by instantiating, invoking, and querying smart contracts.`,
+  `Andromeda Protocol is made up of free, public, and open-source software that is built using the CosmosSDK / CosmWasm tech stack. Anyone can use our platform to build smart contracts, create their own templates to use or sell in the marketplace, create tokens & ADO’s, distribute digital assets, etc. As a result, it is not possible to ensure all aspects of the platform are safe & secure to use.  As with any experimental technology you are using it at your own risk.`,
+  `AS DESCRIBED IN THE DISCLAIMER, ANDROMEDA PROTOCOL IS PROVIDED “AS IS”, AT YOUR OWN RISK, AND WITHOUT ANY WARRANTIES OF ANY KIND. No developer or entity involved in creating Andromeda Protocol will be liable for any claims or damages whatsoever associated with your use, inability to use, or your interaction with other users of the Andromeda Protocol, including any direct, indirect, incidental, special, exemplary, punitive or consequential damages, or loss of profits, tokens, or anything else.`,
 ];
 
 export default DisclaimerModal;
