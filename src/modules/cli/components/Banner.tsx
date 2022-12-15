69e0f37ee7008b646f88b41c57cad797a6d730b0
Leif A Sorensen <60895101+leifasorensen@users.noreply.github.com>
2022-12-15 06:48:49 -0600

    Banner Text Replacement
    - Changed out "Lorem Ipsum" placeholder text with new provided product copy text.
    - Prettier formatting alterations

--- src/modules/cli/components/Banner.tsx
+++ src/modules/cli/components/Banner.tsx
@@ -1,32 +1,55 @@
-import { Box, Button, GridItem, Icon, Image, Link, SimpleGrid, Text } from '@chakra-ui/react'
-import { FileText } from 'lucide-react'
-import React, { FC } from 'react'
+import {
+  Box,
+  Button,
+  GridItem,
+  Icon,
+  Image,
+  Link,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import { FileText } from "lucide-react";
+import React, { FC } from "react";
 
-interface BannerProps {
-
-}
+interface BannerProps {}
 const Banner: FC<BannerProps> = (props) => {
-    const { } = props
+  const {} = props;
 
-    return (
-        <Box background="linear-gradient(90deg, rgba(73,13,152,1) 0%, rgba(2,2,0,1) 50%, rgba(2,96,44,1) 100%)" rounded='2xl'>
-            <SimpleGrid columns={2}>
-                <GridItem>
-                    <Box p='10'>
-                        <Text fontSize='4xl' fontWeight='semibold' >Andromeda CLI</Text>
-                        <Text mt='2' fontSize='md'>
-                            Lorem ipsum dolor sit amet consectetur. Facilisis semper tincidunt pellentesque viverra rhoncus netus.
-                        </Text>
-                        <Button as='a' href="https://docs.andromedaprotocol.io/andromeda/andromeda-cli/introduction" target='_blank' mt='3' leftIcon={<Icon as={FileText} />} colorScheme="primary" size='sm'>Documentation</Button>
-                    </Box>
-                </GridItem>
-                <GridItem alignSelf='end'>
-                    <Box overflow="hidden" roundedBottomEnd='2xl'>
-                        <Image src='/cli/cli.png' w='full' mt='auto' />
-                    </Box>
-                </GridItem>
-            </SimpleGrid>
-        </Box>
-    )
-}
-export default Banner
+  return (
+    <Box
+      background="linear-gradient(90deg, rgba(73,13,152,1) 0%, rgba(2,2,0,1) 50%, rgba(2,96,44,1) 100%)"
+      rounded="2xl"
+    >
+      <SimpleGrid columns={2}>
+        <GridItem>
+          <Box p="10">
+            <Text fontSize="4xl" fontWeight="semibold">
+              Andromeda CLI
+            </Text>
+            <Text mt="2" fontSize="md">
+              Looking to interact with our chain and others? This tool is for
+              you.
+            </Text>
+            <Button
+              as="a"
+              href="https://docs.andromedaprotocol.io/andromeda/andromeda-cli/introduction"
+              target="_blank"
+              mt="3"
+              leftIcon={<Icon as={FileText} />}
+              colorScheme="primary"
+              size="sm"
+            >
+              Documentation
+            </Button>
+          </Box>
+        </GridItem>
+        <GridItem alignSelf="end">
+          <Box overflow="hidden" roundedBottomEnd="2xl">
+            <Image src="/cli/cli.png" w="full" mt="auto" />
+          </Box>
+        </GridItem>
+      </SimpleGrid>
+    </Box>
+  );
+};
+export default Banner;
