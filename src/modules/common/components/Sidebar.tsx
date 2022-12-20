ac5b16faf2024384dedfa43677bf1e008ed006f3
Leif A Sorensen <60895101+leifasorensen@users.noreply.github.com>
2022-12-19 18:52:38 -0600

    Menu Text Changes
    - Textual updates to navigation menu items: App Store and CLI

--- src/modules/common/components/Sidebar.tsx
+++ src/modules/common/components/Sidebar.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, BoxProps, Flex, CloseButton, Image, Divider, Text } from "@chakra-ui/react";
+import {
+  Box,
+  BoxProps,
+  Flex,
+  CloseButton,
+  Image,
+  Divider,
+  Text,
+} from "@chakra-ui/react";
 
 import NavItem from "./NavItem";
 import {
@@ -14,12 +22,12 @@ import { SITE_LINKS } from "../utils/sitelinks";
 import Link from "next/link";
 
 export enum ILinkItemKey {
-  LEARN = 'learn',
-  ADO_BUILDER = 'adobuilder',
-  APP_BUILDER = 'appbuilder',
-  ASSETS = 'assets',
-  APP_STORE = 'appstore',
-  CLI = 'cli',
+  LEARN = "learn",
+  ADO_BUILDER = "adobuilder",
+  APP_BUILDER = "appbuilder",
+  ASSETS = "assets",
+  APP_STORE = "appstore",
+  CLI = "cli",
 }
 interface ILinkItem {
   name: string;
@@ -53,18 +61,18 @@ const LinkItems: ILinkItem[] = [
     key: ILinkItemKey.ASSETS,
   },
   {
-    name: "Dev Store",
+    name: "App Store",
     icon: <SparklesIcon boxSize={5} />,
     href: SITE_LINKS.appStore(),
     key: ILinkItemKey.APP_STORE,
   },
   {
-    name: "Cli",
+    name: "CLI",
     icon: <CliIcon boxSize={5} />,
     href: SITE_LINKS.cli(),
     key: ILinkItemKey.CLI,
   },
-]
+];
 
 interface SidebarProps extends BoxProps {
   onClose: () => void;
@@ -74,42 +82,50 @@ interface SidebarProps extends BoxProps {
 const Sidebar = ({ onClose, activeLink, ...props }: SidebarProps) => {
   return (
     <Box
-      display='flex'
-      flexDirection='column'
+      display="flex"
+      flexDirection="column"
       transition="1s ease"
       bg="dark.50"
       w={60}
       pos="fixed"
       h="full"
-      px='2'
-      py='1'
+      px="2"
+      py="1"
       {...props}
     >
-      <Flex h={20} pl='4' alignItems="center" justifyContent="space-between">
+      <Flex h={20} pl="4" alignItems="center" justifyContent="space-between">
         <Link href={SITE_LINKS.landing()} passHref>
           <a>
-            <Image src="/logo_header.png" w='60%' />
+            <Image src="/logo_header.png" w="60%" />
           </a>
         </Link>
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
       <Box
-        display='flex'
-        flexDir='column'
-        gap='1'
-        flex='1'
+        display="flex"
+        flexDir="column"
+        gap="1"
+        flex="1"
         px={4}
         overflowY="auto"
-        pb='2'
+        pb="2"
       >
         {LinkItems.map((link) => (
-          <NavItem active={link.key === activeLink} gap='1' key={link.key} href={link.href} icon={link.icon}>
+          <NavItem
+            active={link.key === activeLink}
+            gap="1"
+            key={link.key}
+            href={link.href}
+            icon={link.icon}
+          >
             {link.name}
           </NavItem>
         ))}
       </Box>
-      <Divider justifySelf='end' />
-      <Text pl='4' my='2' fontSize='xs' fontWeight='light' fontStyle='light'>Andromeda App 1.5.0</Text>
+      <Divider justifySelf="end" />
+      <Text pl="4" my="2" fontSize="xs" fontWeight="light" fontStyle="light">
+        Andromeda App 1.5.0
+      </Text>
     </Box>
   );
 };
