24aac2a42e68d850290343a2affe41317e7f6ef8
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2022-12-23 22:58:22 +0530

    Added mdx support for blogs in learn section

---
+++ src/modules/learn/components/BlogLayout.tsx
@@ -0,0 +1,98 @@
+import { Layout } from '@/modules/common'
+import { ILinkItemKey } from '@/modules/common/components/Sidebar'
+import { Box, Code, Flex, Image, Text, TextProps, VStack } from '@chakra-ui/react'
+import { MDXProvider } from '@mdx-js/react'
+import React, { FC, useEffect, useMemo, useRef, useState } from 'react'
+import { useHeadsObserver } from './hooks/useHeadObserver'
+
+const components = {
+    img: Image,
+    h1: ({ children }: TextProps) => <Text as='h1' fontWeight='extrabold' fontSize='2xl'>{children}</Text>,
+    h2: ({ children }: TextProps) => <Text as='h2' fontWeight='black' fontSize='xl'>{children}</Text>,
+    h3: ({ children }: TextProps) => <Text as='h3' fontWeight='medium' fontSize='lg'>{children}</Text>,
+    p: ({ children }: TextProps) => <Text as='p'>{children}</Text>,
+    pre: Code,
+    code: Code,
+}
+
+interface BlogLayoutProps {
+    meta: {
+        title: string;
+        author: string;
+    }
+}
+const BlogLayout: FC<BlogLayoutProps> = (props) => {
+    const { children, meta } = props
+    const [headings, setHeadings] = useState<IHeadingListItem[]>([])
+    const contentRef = useRef<HTMLDivElement>(null)
+
+    const headingElems = useMemo(() => {
+        return headings.map(heading => heading.elem)
+    }, [headings])
+
+    const { activeId } = useHeadsObserver(headingElems)
+
+    useEffect(() => {
+        if (contentRef.current) {
+            const elems: IHeadingListItem[] = Array.from(contentRef.current.querySelectorAll('h1, h2, h3'))
+                .map((elem, idx) => {
+                    elem.id = encodeURIComponent(`${elem.innerHTML}-${idx}`)
+                    return {
+                        text: elem.innerHTML,
+                        tag: elem.tagName as IHeadingListItem['tag'],
+                        elem: elem,
+                        id: elem.id
+                    }
+                })
+            setHeadings(elems)
+        }
+    }, [contentRef.current])
+
+
+    return (
+        <MDXProvider components={components}>
+            <Layout activeLink={ILinkItemKey.LEARN}>
+                <Box listStylePosition='inside'>
+                    <Text as='h1' fontWeight='extrabold' fontSize='4xl'>
+                        {meta.title}
+                    </Text>
+                    <Flex direction='row' gap='10' alignItems='start' position='relative' mt='10'>
+                        <VStack flexShrink={0} w='60' overflow='auto' alignItems='stretch' gap='2' position='sticky' top='4' py='4' bg='dark.100' rounded='xl'>
+                            {headings.map((heading) => {
+                                const active = activeId === heading.id
+                                return (
+                                    <Text
+                                        key={heading.id}
+                                        onClick={() => {
+                                            heading.elem.scrollIntoView({ 'behavior': 'smooth' })
+                                        }}
+                                        cursor='pointer'
+                                        px='4'
+                                        fontWeight={active ? 'bold' : 'normal'}
+                                        borderLeftWidth='4px'
+                                        borderColor={active ? 'dark.500' : 'transparent'}
+                                    >
+                                        {heading.text}
+                                    </Text>
+                                )
+                            })}
+
+                        </VStack>
+                        <VStack mb='10' alignItems='stretch' gap='2' ref={contentRef} pl='2' flex='1'>
+                            {children}
+                        </VStack>
+                    </Flex>
+                </Box>
+            </Layout>
+        </MDXProvider>
+    )
+}
+
+interface IHeadingListItem {
+    text: string;
+    tag: 'H1' | 'H2' | 'H3',
+    elem: Element,
+    id: string;
+}
+
+export default BlogLayout
--- src/pages/learn/index.tsx
+++ src/pages/learn/index.tsx
@@ -1,12 +1,9 @@
 import { Layout } from "@/modules/common";
-import PagePlaceholder from "@/modules/common/components/PagePlaceholder";
 import { ILinkItemKey } from "@/modules/common/components/Sidebar";
 import { LearnPage } from "@/modules/learn";
 import type { NextPage } from "next";
 
 const Page: NextPage = () => {
-  //const noRef = null; // alternate to noreferrer
-
   return (
     <Layout activeLink={ILinkItemKey.LEARN}>
       <LearnPage />
