9e55dde37eae549e09052fb68e6ef533e3a62bc9
Leif A Sorensen <60895101+leifasorensen@users.noreply.github.com>
2023-01-30 21:00:21 -0500

    ADO / Module Listing
    Setup of ADO contents and App store templates to only be available for use Case 1 (Auction based marketplace with: CW721, Auction, Splitter, Rates, AddressList)
    
    -Set Blank Canvas Selection Options to use case 1 contents
    - Set App store content to show only auction based marketplace
    -- added splitter as an optional selection for "Auction Based Marketplace" template
    -Set temporary version tag in interface to reflect internal testinng /review
    
    Next Steps:
    - Load App Builder branch (in current state) to this branch for use case testing by team
    - Setup new Learn section based landing page (awaiting design by Abdel)

--- src/lib/schema/templates/index.ts
+++ src/lib/schema/templates/index.ts
@@ -24,37 +24,54 @@ const APP_TEMPLATES: ITemplate[] = [
       },
     ],
 
+    // modules: [
+    //   ...BASE_ADOS.map((ado) => ({ path: ado.source })),
+    //   ...MODULES.map((ado) => ({ path: ado.source })),
+    //   ...PRIMITIVES.map((ado) => ({ path: ado.source })),
+    // ],
     modules: [
-      ...BASE_ADOS.map((ado) => ({ path: ado.source })),
-      ...MODULES.map((ado) => ({ path: ado.source })),
-      ...PRIMITIVES.map((ado) => ({ path: ado.source })),
+      {
+        path: "cw721/latest/cw721",
+      },
+      {
+        path: "auction/latest/auction",
+      },
+      {
+        path: "splitter/latest/splitter",
+      },
+      {
+        path: "rates/latest/rates",
+      },
+      {
+        path: "address-list/latest/address-list",
+      },
     ],
     system: true,
     starter: true,
   },
-  {
-    id: "crowdfund",
-    adoType: "app",
-    name: "Crowdfund App",
-    description:
-      "Setup a crowdfund to distribute NFTs representing fractionalized ownership of the goal being funded. Proceeds are able to be divided and distributed to yield vaults to assure direct delivery to the respective manufacturing / distribution / business partnerships.",
-    opts: ["Crowdfund", "CW721", "Vault", "Rates"],
-    ados: [
-      {
-        path: IImportantAdoKeys.PUBLISH_SETTINGS,
-        id: IImportantAdoKeys.PUBLISH_SETTINGS,
-        required: true,
-      },
-      { path: "cw721/latest/cw721", id: "tokens", required: true },
-      { path: "crowdfund/latest/crowdfund", id: "crowdfund", required: true },
-      { path: "vault/latest/vault", id: "vault", required: true },
-      { path: "rates/latest/rates", id: "rates", required: true },
-    ],
-    modules: [{ path: "vault/latest/vault" }],
-    icon: "/app-templates/icons/crowdfund.png",
-    installed: true,
-    starter: true,
-  },
+  // {
+  //   id: "crowdfund",
+  //   adoType: "app",
+  //   name: "Crowdfund App",
+  //   description:
+  //     "Setup a crowdfund to distribute NFTs representing fractionalized ownership of the goal being funded. Proceeds are able to be divided and distributed to yield vaults to assure direct delivery to the respective manufacturing / distribution / business partnerships.",
+  //   opts: ["Crowdfund", "CW721", "Vault", "Rates"],
+  //   ados: [
+  //     {
+  //       path: IImportantAdoKeys.PUBLISH_SETTINGS,
+  //       id: IImportantAdoKeys.PUBLISH_SETTINGS,
+  //       required: true,
+  //     },
+  //     { path: "cw721/latest/cw721", id: "tokens", required: true },
+  //     { path: "crowdfund/latest/crowdfund", id: "crowdfund", required: true },
+  //     { path: "vault/latest/vault", id: "vault", required: true },
+  //     { path: "rates/latest/rates", id: "rates", required: true },
+  //   ],
+  //   modules: [{ path: "vault/latest/vault" }],
+  //   icon: "/app-templates/icons/crowdfund.png",
+  //   installed: true,
+  //   starter: true,
+  // },
   {
     id: "auction-market",
     adoType: "app",
@@ -80,6 +97,9 @@ const APP_TEMPLATES: ITemplate[] = [
         path: "auction/latest/auction",
       },
       {
+        path: "splitter/latest/splitter",
+      },
+      {
         path: "rates/latest/rates",
       },
       {
@@ -90,72 +110,72 @@ const APP_TEMPLATES: ITemplate[] = [
     installed: true,
     starter: true,
   },
-  {
-    id: "market",
-    adoType: "app",
-    name: "Flat Rate Marketplace",
-    description:
-      "Setup a customizable NFT Marketplace/store to sell NFT’s for a specific price point. NFT’s can be sorted and filtered upon by many different attributes of the NFT’s themselves. The rates and address (Whitelist/Blacklist) list are also configurable and can be modified per marketplace instance. Proceeds will be collected and sent to the marketplace owner and NFT sent to the specific recipient of the purchase.",
-    opts: ["Marketplace", "CW721", "Rates"],
-    ados: [
-      {
-        path: IImportantAdoKeys.PUBLISH_SETTINGS,
-        id: IImportantAdoKeys.PUBLISH_SETTINGS,
-        required: true,
-      },
-      { path: "cw721/latest/cw721", id: "tokens", required: true },
-      {
-        path: "marketplace/latest/marketplace",
-        id: "marketplace",
-        required: true,
-      },
-      { path: "rates/latest/rates", id: "rates", required: false },
-      {
-        path: "address-list/latest/address-list",
-        id: "whitelist",
-        required: false,
-        enabled: false,
-      },
-    ],
-    modules: [
-      { path: "cw721/latest/cw721" },
-      { path: "marketplace/latest/marketplace" },
-      { path: "rates/latest/rates" },
-      { path: "address-list/latest/address-list" },
-    ],
-    icon: "/app-templates/icons/market.png",
-    installed: true,
-    starter: true,
-  },
-  {
-    id: "cw20-staking",
-    adoType: "app",
-    name: "CW20 Token Staking",
-    description:
-      " Setup a means of offering the ability to stake a specific coin and have associated rewards that are aligned to duration of the time of staking. Multiple staking timeframes can be set up with varying percentages of staking reward benefits. The ability to unstake or lock users in is also present.",
-    opts: ["CW20", "CW20 Staking"],
-    ados: [
-      {
-        path: IImportantAdoKeys.PUBLISH_SETTINGS,
-        id: IImportantAdoKeys.PUBLISH_SETTINGS,
-        required: true,
-      },
-      { path: "cw20/latest/cw20", id: "tokens", required: true },
-      {
-        path: "cw20-staking/latest/cw20-staking",
-        id: "staking",
-        required: true,
-      },
-    ],
-    modules: [
-      { path: "cw20/latest/cw20" },
-      { path: "cw20-staking/latest/cw20-staking" },
-      { path: "address-list/latest/address-list" },
-    ],
-    icon: "/app-templates/icons/cw20-staking.png",
-    installed: true,
-    starter: true,
-  },
+  // {
+  //   id: "market",
+  //   adoType: "app",
+  //   name: "Flat Rate Marketplace",
+  //   description:
+  //     "Setup a customizable NFT Marketplace/store to sell NFT’s for a specific price point. NFT’s can be sorted and filtered upon by many different attributes of the NFT’s themselves. The rates and address (Whitelist/Blacklist) list are also configurable and can be modified per marketplace instance. Proceeds will be collected and sent to the marketplace owner and NFT sent to the specific recipient of the purchase.",
+  //   opts: ["Marketplace", "CW721", "Rates"],
+  //   ados: [
+  //     {
+  //       path: IImportantAdoKeys.PUBLISH_SETTINGS,
+  //       id: IImportantAdoKeys.PUBLISH_SETTINGS,
+  //       required: true,
+  //     },
+  //     { path: "cw721/latest/cw721", id: "tokens", required: true },
+  //     {
+  //       path: "marketplace/latest/marketplace",
+  //       id: "marketplace",
+  //       required: true,
+  //     },
+  //     { path: "rates/latest/rates", id: "rates", required: false },
+  //     {
+  //       path: "address-list/latest/address-list",
+  //       id: "whitelist",
+  //       required: false,
+  //       enabled: false,
+  //     },
+  //   ],
+  //   modules: [
+  //     { path: "cw721/latest/cw721" },
+  //     { path: "marketplace/latest/marketplace" },
+  //     { path: "rates/latest/rates" },
+  //     { path: "address-list/latest/address-list" },
+  //   ],
+  //   icon: "/app-templates/icons/market.png",
+  //   installed: true,
+  //   starter: true,
+  // },
+  // {
+  //   id: "cw20-staking",
+  //   adoType: "app",
+  //   name: "CW20 Token Staking",
+  //   description:
+  //     " Setup a means of offering the ability to stake a specific coin and have associated rewards that are aligned to duration of the time of staking. Multiple staking timeframes can be set up with varying percentages of staking reward benefits. The ability to unstake or lock users in is also present.",
+  //   opts: ["CW20", "CW20 Staking"],
+  //   ados: [
+  //     {
+  //       path: IImportantAdoKeys.PUBLISH_SETTINGS,
+  //       id: IImportantAdoKeys.PUBLISH_SETTINGS,
+  //       required: true,
+  //     },
+  //     { path: "cw20/latest/cw20", id: "tokens", required: true },
+  //     {
+  //       path: "cw20-staking/latest/cw20-staking",
+  //       id: "staking",
+  //       required: true,
+  //     },
+  //   ],
+  //   modules: [
+  //     { path: "cw20/latest/cw20" },
+  //     { path: "cw20-staking/latest/cw20-staking" },
+  //     { path: "address-list/latest/address-list" },
+  //   ],
+  //   icon: "/app-templates/icons/cw20-staking.png",
+  //   installed: true,
+  //   starter: true,
+  // },
 ];
 
 export default APP_TEMPLATES;
