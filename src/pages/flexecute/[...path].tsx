3e527c0ed25d3868f135cfb930b9e2c6c8f84373
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-02-16 19:55:05 +0530

    Added proxy toggle

--- src/pages/flexecute/[...path].tsx
+++ src/pages/flexecute/[...path].tsx
@@ -2,26 +2,102 @@ import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { FlexBuilderForm, StagingDocumentsModal } from "@/modules/flex-builder";
 
 import { Box, Flex, Text } from "@/theme/ui-elements";
-import { FileCheckIcon, Layout, PageHeader, truncate } from "@/modules/common";
+import { FileCheckIcon, FilePlusIcon, Layout, PageHeader, truncate } from "@/modules/common";
 import { useRouter } from "next/router";
-import { ITemplate } from "@/lib/schema/types";
+import { IImportantAdoKeys, ITemplate } from "@/lib/schema/types";
 import { useExecuteModal } from "@/modules/modals/hooks";
-import { getADOExecuteTemplate } from "@/lib/schema/utils";
+import { getADOExecuteTemplate, getProxyTemplate } from "@/lib/schema/utils";
 import useConstructADOExecuteMsg from "@/modules/sdk/hooks/useConstructaADOExecuteMsg";
 import { useGetFunds } from "@/modules/sdk/hooks";
 import { useWallet } from "@/lib/wallet";
+import { useEffect, useMemo, useState } from "react";
+import useConstructProxyMsg from "@/modules/sdk/hooks/useConstructProxyMsg";
+import { FormControl, FormLabel, HStack, IconButton, Input, Switch, Tooltip, useToast } from "@chakra-ui/react";
+import { cloneDeep } from "@apollo/client/utilities";
+import { parseJsonFromFile } from "@/lib/json";
+import { parseFlexFile } from "@/lib/schema/utils/flexFile";
+import { DownloadIcon } from "@chakra-ui/icons";
 
 type Props = {
-  template: ITemplate;
+  executeTemplate: ITemplate
+  proxyTemplate: ITemplate
 };
 
-const TemplatePage: NextPage<Props> = ({ template }) => {
+const TemplatePage: NextPage<Props> = ({ executeTemplate, proxyTemplate }) => {
   const router = useRouter();
-  const contract = router.query.contract as string;
-  const construct = useConstructADOExecuteMsg();
+  const toast = useToast({
+    position: "top-right",
+  });
+
+  const ADO_DATA = useMemo(() => {
+    const name = router.query.name as string;
+    const address = router.query.address as string;
+    const appAddress = router.query.appAddress as string;
+    return {
+      name,
+      address,
+      appAddress
+    }
+  }, [router])
+
+  const constructExecuteMsg = useConstructADOExecuteMsg();
+  const constructProxyMsg = useConstructProxyMsg();
+
+  const openExecuteModal = useExecuteModal(ADO_DATA.address);
+  const openProxyModal = useExecuteModal(ADO_DATA.appAddress);
+
   const getFunds = useGetFunds();
-  const openModal = useExecuteModal(contract);
   const account = useWallet();
+
+
+  const [toggleProxy, setToggleProxy] = useState(false)
+  const [modifiedTemplate, setModifiedTemplate] = useState(executeTemplate);
+
+  useEffect(() => {
+    if (!toggleProxy) {
+      setModifiedTemplate(executeTemplate)
+      return;
+    };
+    const newTemplate = cloneDeep(proxyTemplate);
+    const formData = newTemplate.formData ?? {};
+    formData[IImportantAdoKeys.PROXY_MESSAGE] = {
+      ...(formData[IImportantAdoKeys.PROXY_MESSAGE] ?? {}),
+      parent: ADO_DATA.appAddress,
+      component_name: ADO_DATA.name,
+    };
+    newTemplate.formData = formData;
+    setModifiedTemplate(newTemplate);
+  }, [ADO_DATA, toggleProxy, executeTemplate, proxyTemplate]);
+
+  const handleFlexInput = async (file: File) => {
+    try {
+      const json = await parseJsonFromFile(file);
+      const _template = await parseFlexFile(json);
+
+      const clone = cloneDeep(modifiedTemplate);
+      clone.ados.forEach((ado) => {
+        if (ado.id === IImportantAdoKeys.PROXY_MESSAGE) return;
+        if (!_template.formData?.[ado.id]) throw new Error("Wrong flex file");
+        if (clone.formData) {
+          clone.formData[ado.id] = {
+            ...clone.formData[ado.id],
+            ..._template.formData?.[ado.id],
+          };
+        }
+      });
+      setModifiedTemplate(clone);
+      toast({
+        title: "Import successfull",
+        status: "success",
+      });
+    } catch (err) {
+      toast({
+        title: "Error while importing",
+        status: "error",
+      });
+    }
+  };
+
 
   const handleSubmit = async (
     {
@@ -30,64 +106,77 @@ const TemplatePage: NextPage<Props> = ({ template }) => {
       formData: any;
     }
   ) => {
-    const msg = construct(formData);
-    // console.log(msg);
     const funds = getFunds(formData);
-    openModal(msg, funds);
+    if (toggleProxy) {
+      const msg = constructProxyMsg(formData);
+      // console.log(msg, "MSG")
+      openProxyModal(msg, funds);
+    } else {
+      const msg = constructExecuteMsg(formData);
+      // console.log(msg, "MSG")
+      openExecuteModal(msg, funds);
+    }
   };
 
-  //TODO: Setup staging availability flags for loading staging sections if passed
-  const staging_available = false;
+  const InputElement = useMemo(
+    () => (
+      <HStack spacing={4}>
+        <Box>
+          <FormControl display='flex' alignItems='center' color='dark.500'>
+            <FormLabel htmlFor='use-proxy' mb='0' fontSize='sm'>
+              Use Proxy?
+            </FormLabel>
+            <Switch isChecked={toggleProxy} onChange={(e) => setToggleProxy(e.target.checked)} id='use-proxy' />
+          </FormControl>
+        </Box>
+        <Box>
+          <Tooltip label='Import Staging' color='dark.500'>
+            <IconButton
+              as="label"
+              htmlFor="flexecute-file-input"
+              variant="outline"
+              aria-label="flex-input"
+              cursor="pointer"
+              icon={<FilePlusIcon boxSize={5} color="gray.500" />}
+            />
+          </Tooltip>
+          <Input
+            onChange={(e) => {
+              const file = e.target.files?.item(0);
+              if (file) {
+                handleFlexInput(file);
+              }
+            }}
+            multiple={false}
+            type="file"
+            id="flexecute-file-input"
+            // Only Allow flex file
+            accept=".flex"
+            srOnly
+          />
+        </Box>
+      </HStack>
+    ),
+    [handleFlexInput, toggleProxy],
+  );
+
+  const UPDATE_KEY = useMemo(() => Math.random(), [modifiedTemplate]);
+
 
   return (
     <Layout>
-      <PageHeader title={`Execute message`} desc={`App address ${contract}`} />
+      <PageHeader
+        title={modifiedTemplate.name || `Execute message`}
+        desc={modifiedTemplate.description || `App address ${ADO_DATA.address}`}
+        rightElement={InputElement}
+      />
 
       <Box mt={10}>
-        {/* Staging section to be shown when declared */}
-        {/* ToDO: Modularize as component? */}
-        {staging_available && (
-          <Flex
-            align="center"
-            border="1px solid"
-            borderColor="primary.300"
-            borderRadius="lg"
-            bg="primary.25"
-            mb={4}
-            p={3}
-          >
-            <Box mr={4}>
-              <Flex
-                justify="center"
-                align="center"
-                bg="primary.100"
-                color="primary.700"
-                borderRadius="full"
-                p={3}
-              >
-                <FileCheckIcon boxSize={6} />
-              </Flex>
-            </Box>
-
-            <Box flex={1}>
-              <Text color="primary.700" fontWeight={500}>
-                Staging Available
-              </Text>
-              <Text color="primary.600" fontSize="sm">
-                Need to perform this operation multiple times? Staging documents
-                are available.
-              </Text>
-            </Box>
-
-            <StagingDocumentsModal />
-          </Flex>
-        )}
-        {/* end of toggleable staging section */}
         <FlexBuilderForm
           // ID is required to refresh the component after we modify the template. If not provided,
           // form will not populate name and address field on direct visit to the page
-          key={template.name}
-          template={template}
+          key={UPDATE_KEY}
+          template={modifiedTemplate}
           onSubmit={handleSubmit}
           notReady={!account}
           addButtonTitle="Add Attachment"
@@ -107,14 +196,18 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<Props> = async (ctx) => {
   const { params } = ctx;
   const path = params?.path as string[];
-  const data = await getADOExecuteTemplate(path.join("/"));
-  if (!data) {
+  const executeTemplate = await getADOExecuteTemplate(path.join("/"));
+  const proxyTemplate = await getProxyTemplate(path.join("/"));
+  if (!executeTemplate || !proxyTemplate) {
     return {
       notFound: true,
     };
   }
   return {
-    props: { template: JSON.parse(JSON.stringify(data)) },
+    props: {
+      executeTemplate: JSON.parse(JSON.stringify(executeTemplate)),
+      proxyTemplate: JSON.parse(JSON.stringify(proxyTemplate))
+    },
     revalidate: 300,
   };
 };
