b6b8c33ab67272e2e71f22985ed904ca1f3a6ab3
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-03-29 23:07:12 +0530

    Updated use-case 1 temmplate and added home button

--- src/modules/app-canvas/topbar/index.tsx
+++ src/modules/app-canvas/topbar/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react'
-import { Box, Flex, HStack } from '@chakra-ui/react';
+import { Box, Flex, HStack, Icon, IconButton } from '@chakra-ui/react';
 import AppNameButton from '../common/appName';
 import PublishButton from '../common/publish';
-import { Wallet } from '@/modules/common';
+import { AndromedaIcon, Wallet } from '@/modules/common';
 import DownloadFlexButton from '../common/downloadFlex';
 import ImportFlexButton from '../common/importFlex';
 import Toolbar from '../toolbar';
+import { TmpButton } from '@/theme/new-system-tmp/ui-elements';
+import Link from 'next/link';
+import { SITE_LINKS } from '@/modules/common/utils/sitelinks';
 
 interface TopBarProps {
 
@@ -16,6 +19,11 @@ const TopBar: FC<TopBarProps> = (props) => {
     return (
         <Flex direction='row' px='2' py='1.5'>
             <HStack flex={1}>
+                <Link href={SITE_LINKS.flexBuilderHome()} passHref>
+                    <TmpButton as='a'>
+                        <Icon as={AndromedaIcon} boxSize='4' />
+                    </TmpButton>
+                </Link>
                 <AppNameButton />
                 <ImportFlexButton />
                 <Toolbar />
