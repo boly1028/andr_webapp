9e746415ccb6e5911d0e943e3c9fdf0d7abe20c7
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-04-03 20:56:29 +0530

    Disable publish if app is not dirty

--- src/modules/app-canvas/common/publish.tsx
+++ src/modules/app-canvas/common/publish.tsx
@@ -2,6 +2,7 @@ import { useWallet } from '@/lib/wallet'
 import { TmpButton } from '@/theme/new-system-tmp/ui-elements'
 import { Button, ButtonProps } from '@chakra-ui/react'
 import React, { FC } from 'react'
+import { useAppBuilder } from '../canvas/Provider'
 import { usePublish } from '../hooks/usePublish'
 
 interface PublishButtonProps extends ButtonProps {
@@ -9,10 +10,11 @@ interface PublishButtonProps extends ButtonProps {
 }
 const PublishButton: FC<PublishButtonProps> = (props) => {
     const { } = props
+    const { isDirty } = useAppBuilder()
     const { publishAppWithAppRename: publish } = usePublish()
     const wallet = useWallet()
     return (
-        <Button isDisabled={!wallet?.address} as={TmpButton} size='sm' colorScheme='primary' {...props} onClick={publish}>
+        <Button isDisabled={!wallet?.address || !isDirty} as={TmpButton} size='sm' colorScheme='primary' {...props} onClick={publish}>
             Publish
         </Button>
     )
