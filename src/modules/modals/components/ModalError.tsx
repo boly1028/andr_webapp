caab0b19dcbe4b62f998170c422a193f9e7073ae
VivekXcubeAd <vivek.jaiswal@xcubelabs.com>
2023-08-18 19:14:19 +0530

    Change in error modal 2.0

--- src/modules/modals/components/ModalError.tsx
+++ src/modules/modals/components/ModalError.tsx
@@ -1,6 +1,6 @@
-// import { truncate } from "@/modules/common";
+import { truncate } from "@/modules/common";
 import { InfoIcon } from "@chakra-ui/icons";
-import { Box, Button, Center, Flex, IconButton, Text, Textarea } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Text, Textarea } from "@chakra-ui/react";
 import { AlertCircle, Copy } from "lucide-react";
 import { FC, memo } from "react";
 import { toast } from "react-toastify";
@@ -11,11 +11,7 @@ const ModalError: FC = memo(function ModalError({ children }) {
 
   const onReport = () => {
     close();
-  };
-  
-  const onCancel = () => {
-    close();
-    setError();
+    // setError();
   };
 
   const onCopy = () => {
@@ -57,42 +53,23 @@ const ModalError: FC = memo(function ModalError({ children }) {
         fontSize={'20px'}
         sx={{ textAlign: "center", fontWeight: "bold" }}
       >
-        Something went wrong
+        Unknown Request
       </Text>
       {/* <Text mt="20px" textAlign='center' sx={{ fontWeight: 400, color: "dark.500" }}>
         {error.message.length > 100
           ? truncate(error.message, [25, 50])
           : error.message}
       </Text> */}
-      <Flex w='full' mt='10px' color='dark.500'>
-        <Text fontWeight={'bold'} fontSize='16px'>Error :</Text>
-        <Text fontSize='16px'> &nbsp;Unknown Request</Text>
-      </Flex>
-      {error.message &&
-        <Flex direction={'column'} w='full' rowGap={'5px'}>
-          <Text w='full' color='dark.500'>Error details</Text>
-          <Flex columnGap={'6px'} alignItems='end'>
-            <Textarea
-              value={error.message}
-              fontSize={'16px'}
-              color='dark.500'
-              backgroundColor={'dark.100'}
-              resize='none'
-              p='6px'
-              size='sm'
-              rows={2}
-            />
-            <IconButton
-              aria-label='Copy Error details'
-              icon={<Copy />}
-              size='sm'
-              colorScheme={'primary'}
-              variant='solid'
-              onClick={onCopy}
-            />
-          </Flex>
-        </Flex>
-      }
+      <Textarea
+        // placeholder='Here is a sample placeholder'
+        value={error.message}
+        fontSize={'16px'}
+        color='dark.500'
+        backgroundColor={'dark.100'}
+        resize='none'
+        mt="20px"
+        pb='10px'
+      />
       <Flex
         bgColor={'rgba(255, 160, 70, 0.12)'}
         p={'12px 20px'}
@@ -104,7 +81,8 @@ const ModalError: FC = memo(function ModalError({ children }) {
           <InfoIcon boxSize={3.5} color='rgba(255, 183, 130, 1)' />
         </Box>
         <Text fontSize={'16px'}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis perspiciatis quaerat optio ducimus aut! Iusto.        </Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis perspiciatis quaerat optio ducimus aut! Iusto.
+        </Text>
       </Flex>
       <Flex w="full" justifyContent={'end'}>
         <Button
@@ -117,22 +95,12 @@ const ModalError: FC = memo(function ModalError({ children }) {
         >
           Report
         </Button>
-        <Button
-          variant="outline"
-          sx={{ fontSize: "16px", padding: "10px 32px" }}
-          onClick={onCancel}
-          mt="40px"
-          mr="10px"
-        >
-          Cancel
-        </Button>
-        {/* {error.message.length > 100 && (
+        {error.message && (
           <Button
             variant="solid"
             sx={{
               fontSize: "16px",
               padding: "10px 16px",
-              "&:hover": { colorScheme: 'primary' }
             }}
             onClick={onCopy}
             mt="40px"
@@ -142,7 +110,7 @@ const ModalError: FC = memo(function ModalError({ children }) {
           >
             Copy
           </Button>
-        )} */}
+        )}
       </Flex>
     </Box>
   );
