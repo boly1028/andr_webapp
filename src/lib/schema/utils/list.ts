e107b6f51fff3b2014c7d2669000cbb7b12685de
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-09-14 22:50:10 +0530

    Remove unnecessary array loop for include ado

--- src/lib/schema/utils/list.ts
+++ src/lib/schema/utils/list.ts
@@ -2,18 +2,16 @@
  * This file contains list for schema and provide a neccessary wrapper for json imports. This way we can reference the
  * variables exported from this module and adjust json in one place
  */
-import { IAdoType } from '../types'
 import { MASTER_ADOENABLE, MASTER_BASEADO, MASTER_MODIFIER, MASTER_MODULE, MASTER_PRIMITIVE, MASTER_QUERY, MASTER_RECEIVE, MASTER_RESPONSE, MASTER_SYSTEM } from './masterList';
 
 /** Skipping base ados which are not yet ready for processing. Instead of removing these from schema parser
  * We can disable it here, so they will still be availaible for testing purpose if directly enabled
  */
-export const INCLUDE_ADO: string[] = Object.keys(MASTER_ADOENABLE).filter(ado => MASTER_ADOENABLE[ado] === true);
 
-export const BASE_ADOS = MASTER_BASEADO.filter(ado => INCLUDE_ADO.includes(ado.$id as IAdoType));
+export const BASE_ADOS = MASTER_BASEADO.filter(ado => MASTER_ADOENABLE[ado.$id]);
 export const MODIFIERS = MASTER_MODIFIER;
-export const MODULES = MASTER_MODULE.filter(ado => INCLUDE_ADO.includes(ado.$id as IAdoType));
-export const PRIMITIVES = MASTER_PRIMITIVE.filter(ado => INCLUDE_ADO.includes(ado.$id as IAdoType));
+export const MODULES = MASTER_MODULE.filter(ado => MASTER_ADOENABLE[ado.$id]);
+export const PRIMITIVES = MASTER_PRIMITIVE.filter(ado => MASTER_ADOENABLE[ado.$id]);
 export const RECEIVES = MASTER_RECEIVE;
 export const QUERIES = MASTER_QUERY;
 export const QUERY_RESPONSES = MASTER_RESPONSE;
